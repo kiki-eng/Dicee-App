ad24df48c8ae27392e0cdf5dbcc1de13da10d803
kiki-eng <abosedekikelomo408@gmail.com>
2023-02-13 17:24:25 -0500

    Initial Commit

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -9,12 +9,39 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+//IBOutlet allows me to reference a UI Element
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImage2: UIImageView!
+   
+   
+    
+   
+    
+        //WHO          WHAT     //VALUE
+        
+  
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBAction func rollButtonPress(_ sender: UIButton) {
+        
+        //Putting my array in a constant variable
+        
+        let diceArray = [UIImage(imageLiteralResourceName: "DiceOne"),
+                         UIImage(imageLiteralResourceName: "DiceTwo"),
+                         UIImage(imageLiteralResourceName: "DiceThree"),
+                         UIImage(imageLiteralResourceName: "DiceFour"),
+                         UIImage(imageLiteralResourceName: "DiceFive"),
+                         UIImage(imageLiteralResourceName: "DiceSix")]
+       
+       /*setting my array to my diceImageView1 and 2 and given
+        it a random number method to generate random number for me
+        We can use both .randomElement() or the other one i use*/
+        
+        diceImageView1.image = diceArray[Int.random(in: 0...5)]
+        diceImage2.image = diceArray[Int.random(in: 0...5)]
+        
+        
+        
     }
-
-
 }
 
